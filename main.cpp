ebc20f6fcf786de4ab39628d298dd0cd2b10fffe
dianhsu <50162344+dianhsu@users.noreply.github.com>
2022-08-09 14:09:51 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define itr(...) begin(__VA_ARGS__), end(__VA_ARGS__)
+#define fastIO() ios::sync_with_stdio(false),cin.tie(nullptr),cout.tie(nullptr)
+#define debug(x...) do { cout << "\033[32;1m" << #x <<" -> "; rd_debug(x); } while (0)
+void rd_debug() { cout << "\033[39;0m" << endl; }
+template<class T, class... Ts> void rd_debug(const T& arg,const Ts&... args) { cout << arg << " "; rd_debug(args...); }
+#define pb push_back
+#define PF(x) ((x) * (x))
+#define LF(x) ((x) * PF(x))
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+
+const double eps = 1e-6;
+const int MOD = 1e9 + 7;
+const int inf = 0x3f3f3f3f;
+const ll infl = 0x3f3f3f3f3f3f3f3fll;
+
+int main() {
+  fastIO();
+
+  return 0;
+}
