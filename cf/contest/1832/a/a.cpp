82b10caa5091789c94a3e529c2e370b027726a16
dianhsu <dianxu@microsoft.com>
2023-08-04 02:10:16 +0000

    update

---
+++ cf/contest/1832/a/a.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define sx(x) ((x) * (x))
+#define vx(x) (sx(x) * (x))
+#define itr(x) begin(x), end(x)
+#define debug(x...)                                                            \
+  do {                                                                         \
+    cout << "\033[32;1m" << #x << " -> ";                                      \
+    rd_debug(x);                                                               \
+  } while (0)
+
+void rd_debug() { cout << "\033[39;0m" << endl; }
+
+template <class T, class... Ts> void rd_debug(const T& arg, const Ts &...args) {
+  cout << arg << " ";
+  rd_debug(args...);
+}
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+
+const double eps = 1e-7;
+const int MOD1 = 1e9 + 7;
+const int MOD9 = 998244353;
+const int inf = 0x3f3f3f3f;
+const ll infl = 0x3f3f3f3f3f3f3f3fll;
+
+int __INIT_IO__ = [](){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  cout << fixed << setprecision(12);
+  return 0;
+}();
+
+int main() {
+  int t;
+  cin >> t;
+  while(t--){
+    string s;
+    cin >> s;
+    map<char, int> mp;
+    for(auto& c: s){
+      mp[c]++;
+    }
+    int cnt = 0;
+    for(auto& [k, v]: mp){
+      if(v > 1) cnt++;
+    }
+    cout << (cnt >= 2 ? "Yes" : "No") << endl;
+  }
+  return 0;
+}
+
