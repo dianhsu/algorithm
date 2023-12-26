e6d5f0294a5f7e0a14518e17bee7b1e5bc290570
dianhsu <xudian.cn@gmail.com>
2023-12-26 16:52:00 +0000

    Update Dockerfile and .gitignore, add Codeforces solution

---
+++ Codeforces/1917/a/code.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define PF(x) ((x) * (x))
+#define LF(x) (PF(x) * (x))
+#define itr(x) begin(x), end(x)
+#define debug(x...)                                                            \
+  do {                                                                         \
+    cout << "\033[32;1m" << #x << " -> ";                                      \
+    rd_debug(x);                                                               \
+  } while (0)
+
+void rd_debug() { cout << "\033[39;0m" << endl; }
+
+template <class T, class... Ts> void rd_debug(const T& arg, const Ts &...args) {
+  cout << arg << " ";
+  rd_debug(args...);
+}
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+
+const double eps = 1e-7;
+const int MOD1 = 1e9 + 7;
+const int MOD9 = 998244353;
+const int inf = 0x3f3f3f3f;
+const ll infl = 0x3f3f3f3f3f3f3f3fll;
+
+int __INIT_IO__ = [](){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  cout << fixed << setprecision(12);
+  return 0;
+}();
+
+int main() {
+  int t;
+  cin >> t;
+  while(t--){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    int cl = 0, cm = 0, cr = 0;
+    for(auto& it: arr){
+      cin >> it;
+      if(it < 0) cl++;
+      else if(it == 0) cm++;
+      else cr++;
+    }
+    if(cm > 0 or cl % 2 == 1){
+      cout << 0 << endl;
+    }else{
+      cout << 1 << endl;
+      cout << 1 << " " << 0 << endl;
+    }
+  }
+
+  return 0;
+}
+
