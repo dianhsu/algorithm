76c91b0b6f06801017c2d9c96716538a7728506b
dianhsu <xudian.cn@gmail.com>
2023-12-29 17:10:33 +0000

    Add input and output files for testing

--- Codeforces - Codeforces Round 918 (Div. 4)/G. Bicycles/sol.cpp
+++ Codeforces - Codeforces Round 918 (Div. 4)/G. Bicycles/sol.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 void rd_debug() { cout << "\033[39;0m" << endl; }
 
-template <class T, class... Ts> void rd_debug(const T& arg, const Ts &...args) {
+template <class T, class... Ts> void rd_debug(const T &arg, const Ts &...args) {
   cout << arg << " ";
   rd_debug(args...);
 }
@@ -28,17 +28,57 @@ const int MOD9 = 998244353;
 const int inf = 0x3f3f3f3f;
 const ll infl = 0x3f3f3f3f3f3f3f3fll;
 
-int __INIT_IO__ = [](){
+int __INIT_IO__ = []() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
   cout << fixed << setprecision(12);
   return 0;
 }();
-
+struct Node {
+  int cu, cs;
+  ll ct;
+};
 int main() {
-
-
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<PII>> g(n + 1);
+    for (int i = 0; i < m; ++i) {
+      int u, v, w;
+      cin >> u >> v >> w;
+      g[u].emplace_back(v, w);
+      g[v].emplace_back(u, w);
+    }
+    vector<int> s(n + 1, 0);
+    for (int i = 1; i <= n; ++i) {
+      cin >> s[i];
+    }
+    vector<vector<ll>> dp(n + 1, vector<ll>(1001, infl));
+    dp[1][s[1]] = 0;
+    queue<Node> q;
+    q.push(Node{1, s[1], 0});
+    while (!q.empty()) {
+      auto [cu, cs, ct] = q.front();
+      q.pop();
+      if (dp[cu][cs] < ct)
+        continue;
+      for (auto [nv, nw] : g[cu]) {
+        ll nt = ct + nw * 1ll * cs;
+        int ns = min(s[nv], cs);
+        if(nt < dp[nv][ns]){
+          dp[nv][ns] = nt;
+          q.push(Node{nv, ns, nt});
+        }
+      }
+    }
+    ll ans = infl;
+    for (int i = 1; i <= 1000; ++i) {
+      ans = min(ans, dp[n][i]);
+    }
+    cout << ans << endl;
+  }
   return 0;
 }
-
--- scripts/linux/run.sh
+++ scripts/linux/run.sh
@@ -5,9 +5,9 @@ for i in `ls *.i.txt`; do
     $1 < $i > $id.a.txt
     diff -Z -y $id.o.txt $id.a.txt > $id.d.txt
     if [ -s $id.d.txt ]; then
-        echo "Failed $id"
-        code $id.d.txt
+        echo -e "Failed \033[0;31m$id\033[0m"
+        code --diff $id.o.txt $id.a.txt
     else
-        echo "Passed $id"
+        echo -e "Passed \033[0;32m$id\033[0m"
     fi
 done
