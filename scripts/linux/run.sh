53c59193468269de780e3301b3a48b2497aa09ac
dianhsu <xudian.cn@gmail.com>
2023-12-30 07:35:08 +0000

    update

--- scripts/linux/run.sh
+++ scripts/linux/run.sh
@@ -1,13 +1,13 @@
-echo $1
+# $1: executable file
 for i in `ls *.i.txt`; do
-    echo $i
     id=`echo $i | cut -d'.' -f1`
+    echo -n "Running test case $id ... "
     $1 < $i > $id.a.txt
-    diff -Z -y $id.o.txt $id.a.txt > $id.d.txt
-    if [ -s $id.d.txt ]; then
-        echo -e "Failed \033[0;31m$id\033[0m"
+    cnt=`diff -Z $id.o.txt $id.a.txt | wc -l`
+    if [ $cnt -gt 0 ]; then
+        echo -e "\033[0;31mFailed\033[0m."
         code --diff $id.o.txt $id.a.txt
     else
-        echo -e "Passed \033[0;32m$id\033[0m"
+        echo -e "\033[0;32mPassed\033[0m."
     fi
 done
