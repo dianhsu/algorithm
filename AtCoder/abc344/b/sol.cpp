38d76b522355fb2c34bd1d0404c5690cb8d783d1
dianhsu <xudian.cn@gmail.com>
2024-06-20 00:05:10 +0000

    update

---
+++ AtCoder/abc344/b/sol.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define PF(x) ((x) * (x))
+#define LF(x) (PF(x) * (x))
+#define itr(x) begin(x), end(x)
+#define debug(x...)                                                            \
+  do {                                                                         \
+    cout << "\033[32;1m" << #x << " -> ";                                      \
+    rd_debug(x);                                                               \
+  } while (0)
+
+void rd_debug() { cout << "\033[39;0m" << endl; }
+
+template <class T, class... Ts> void rd_debug(const T &arg, const Ts &...args) {
+  cout << arg << " ";
+  rd_debug(args...);
+}
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+
+const double eps = 1e-7;
+const int MOD1 = 1e9 + 7;
+const int MOD9 = 998244353;
+const int inf = 0x3f3f3f3f;
+const ll infl = 0x3f3f3f3f3f3f3f3fll;
+
+int __INIT_IO__ = []() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  cout << fixed << setprecision(12);
+  return 0;
+}();
+
+int main() {
+  vector<int> arr;
+  int tv;
+  while (cin >> tv) {
+    arr.push_back(tv);
+  }
+  reverse(itr(arr));
+  for (auto& it: arr) {
+    cout << it << endl;
+  }
+  return 0;
+}
